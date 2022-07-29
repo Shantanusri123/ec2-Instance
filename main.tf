25a1fa916b9d1e4fbe3390ed9576f191319cfe4a
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-29 13:33:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "ec2vm" {
-    ami = var.aws_ami
-    instance_type = var.aws_instance
+  ami = var.aws_ami
+  instance_type = var.aws_instance
+  
+  tags = {
+    Name = var.aws_name
+  }
 }

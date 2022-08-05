d568228d072f7986ec3c2826403a4b7a8cd43105
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-08-05 15:02:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*data "terraform_remote_state" "security_group_info"{
+data "terraform_remote_state" "security_group_info"{
   backend = "s3"
   config = {
     bucket               = "terraform-state-for-us-again"
@@ -6,12 +6,12 @@
     region  = var.aws_region
     profile = "aws_profile_dev"
   }
-}*/
+}
 
 resource "aws_instance" "ec2vm" {
   ami = var.aws_ami
   instance_type = var.aws_instance
- // vpc_security_group_ids = [data.terraform_remote_state.security_group_info.outputs.sg_id]
+  vpc_security_group_ids = [data.terraform_remote_state.security_group_info.outputs.sg_id]
   tags = {
     Name = var.aws_name
   }
